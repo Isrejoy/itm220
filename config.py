f6c8f469e6993e50d6101e674cb8bf540d62181a
Isrejoy <zhoushengjian1@gmail.com>
2025-07-16 23:24:17 -0600

    done

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 # Path to the SQLite database file
-DB_PATH = "database.sqlite"
+DB_PATH = "minecraft_mod.sqlite"
 
 # Table name to interact with
-TABLE_NAME = "your_table_name"
+TABLE_NAME = "mcmod"
 
 # Columns to manage (excluding ID)
 COLUMNS = {
-    "name": "TEXT",
-    "email": "TEXT",
-    "age": "INTEGER"
+    "mcmod_id": "INT(10)",
+    "mcmod_name": "VARCHAR(45)",
+    "create_date": "DATE",
+    "author_id": "INT(10)"
 }
